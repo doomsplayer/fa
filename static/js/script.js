327571f2cf7cf75ef598ae099ea550c60619b61e
pandada8 <pandada8@gmail.com>
2014-08-05 12:52:00 +0800

    修正ngRoute的版本，添加ngIsotope

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,5 @@
 (function() {
-    var app = angular.module('badmintonhome', []);
-
+    var app = angular.module('badmintonhome', ['ng-isotope']);
     app.directive('badmintonLearn', function() {
         return {
             restrict: 'E',
@@ -24,7 +23,7 @@
     });
     app.directive('mainBar', function(){
         return {
-            // scope: {}, // {} = isolate, true = child, false/undefined = no change
+            scope: {},
             controller: ['$scope',function($scope){
                 // FIXME 使用$resource 替代
                 $scope.titles = [{title:'促销信息',url:'#'},{title:'学打羽毛球',url:'#',subtitle:[{'url': '#', 'title': '大话羽球'}, {'url': '#', 'title': '羽球知识'}, {'url': '#', 'title': '羽球技术'}, {'url': '#', 'title': '羽球战术'}, {'url': '#', 'title': '伤病防护'}]},{'title':'视频库','url':'#','subtitle':[{'url': '#', 'title': '国际大赛专辑'}, {'url': '#', 'title': '经典对战专辑'}, {'url': '#', 'title': '玩转羽球'}]},{'url':'#','title':'联系我们'}]
@@ -33,17 +32,10 @@
                     $('#idmenu').addClass('style_menu');
                 },0)
                 // workaround ...
-                
             }],
-            // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-            restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+            restrict: 'E',
             templateUrl: 'static/tpl/mainBar.html',
             replace: true,
-            // transclude: true,
-            // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
-            link: function($scope, iElm, iAttrs, controller) {
-                
-            }
         };
     });
 })();
