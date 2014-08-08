0d65d549c4e499358dc0b26a1d67a9a0473edf69
pre1ude <1024756836@qq.com>
2014-08-08 15:23:14 +0800

    add video-lib.html and video-album.html

--- static/js/script.js
+++ static/js/script.js
@@ -1,27 +1,35 @@
 (function() {
-    var app = angular.module('badmintonhome',['ngResource','ngRoute']);
+    var app = angular.module('badmintonhome', ['ngResource', 'ngRoute']);
     // URL route
-    app.config(['$routeProvider',function($routeProvider) {
-        $routeProvider.when('/',{
-            templateUrl:'static/tpl/index.html',
-            controller:'IndexCtrl'
-        }).when('/about-us',{
-            templateUrl:'static/tpl/about-us.html',
-            controller:'defaultCtrl'
-        }).when('/portfolio',{
-            templateUrl:"static/tpl/portfolio.html",
-            controller:'PortfolioCtrl'
-        })
-    }])
+    app.config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider.when('/', {
+                templateUrl: 'static/tpl/index.html',
+                controller: 'IndexCtrl'
+            }).when('/about-us', {
+                templateUrl: 'static/tpl/about-us.html',
+                controller: 'defaultCtrl'
+            }).when('/portfolio', {
+                templateUrl: "static/tpl/portfolio.html",
+                controller: 'PortfolioCtrl'
+            })
+        }
+    ])
 
-        
-    app.controller('PortfolioCtrl', ['$scope', function($scope){
-        $rootScope.is_index = false;
-    }]).controller('IndexCtrl', ['$rootScope', function($rootScope){
-        $rootScope.is_index = true;
-    }]).controller('defaultCtrl', ['$rootScope', function($rootScope){
-        $rootScope.is_index = false;       
-    }]).directive('badmintonLearn', function() {
+
+    app.controller('PortfolioCtrl', ['$scope',
+        function($scope) {
+            $rootScope.is_index = false;
+        }
+    ]).controller('IndexCtrl', ['$rootScope',
+        function($rootScope) {
+            $rootScope.is_index = true;
+        }
+    ]).controller('defaultCtrl', ['$rootScope',
+        function($rootScope) {
+            $rootScope.is_index = false;
+        }
+    ]).directive('badmintonLearn', function() {
         return {
             restrict: 'E',
             templateUrl: 'static/tpl/badminton-learn.html'
@@ -42,19 +50,21 @@
 
         }
     });
-    app.directive('mainBar', function(){
+    app.directive('mainBar', function() {
         return {
             scope: {},
-            controller: ['$scope','$http',function($scope,$http){
-                $http.get('/api/common/mainBar').success(function(data,status,headers,config){
-                    $scope.titles = data;
-                    setTimeout(function(){
-                        $('#idmenu').mnmenu();
-                        $('#idmenu').addClass('style_menu');
-                    },2)
+            controller: ['$scope', '$http',
+                function($scope, $http) {
+                    $http.get('/api/common/mainBar').success(function(data, status, headers, config) {
+                        $scope.titles = data;
+                        setTimeout(function() {
+                            $('#idmenu').mnmenu();
+                            $('#idmenu').addClass('style_menu');
+                        }, 2)
 
-                })
-            }],
+                    })
+                }
+            ],
             restrict: 'E',
             templateUrl: 'static/tpl/mainBar.html',
             replace: true,
