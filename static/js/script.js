0f7e7c7115ad3221d3ae81841bc2ad11220b0a3a
pandada8 <pandada8@gmail.com>
2014-08-15 03:58:42 +0800

    add VideoLib in route

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
 (function() {
-    var app = angular.module('badmintonhome', ['ngResource', 'ngRoute']);
+    var app = angular.module('badmintonhome', ['ngResource', 'ngRoute','ui.bootstrap']);
     // URL route
     app.config(['$routeProvider',
         function($routeProvider) {
@@ -32,8 +32,20 @@
         function($rootScope) {
             $rootScope.is_index = false;
         }
-    ]).controller('VideoLibCtrl', ['$rootScope', function($rootScope){
+    ]).controller('VideoLibCtrl', ['$rootScope','$scope', function($rootScope,$scope){
         $rootScope.is_index = false;
+        // FIXME 视频库的API
+        $scope.slides = []
+        var sample = {pic:'../static/img/video-carousel-0.jpg',title:'赠粉丝——李龙大郑在成搭档这七年',content:'韩国羽毛球名将李龙大/郑在成在北京时间2012年8月5日进行的伦敦奥运会羽毛球男双3、4名争夺战中战胜马来西亚组合获得一枚铜牌。伦敦奥运也是郑在成代表韩国参加的最后的比赛，自此他将正式退役。这枚男双铜牌也将是他运动员生涯的最后一枚奖牌。自此，这对创造无数传奇的的搭档也将正式解散。图为李龙大与郑在成获得铜牌后紧紧相拥。'}
+        $scope.slides.push(sample)
+        $scope.slides.push(sample)
+        $scope.slides.push(sample)
+        
+        $scope.videos = []
+        var video = {preview:'../static/img/img_test4.jpg',url:'',title:'假动作的魅力：盖得假动作集锦',desc:'描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述'}
+        for(var i=1;i<7;i++){
+            $scope.videos.push(video)
+        }
         
     }]).directive('badmintonLearn', function() {
         return {
