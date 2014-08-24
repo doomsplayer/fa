fa5e8b41078af0e45d9dc2aafadcc915dc40b3ab
pandada8 <pandada8@gmail.com>
2014-08-24 23:55:42 +0800

    try fix

--- static/js/directive/portfolio.js
+++ static/js/directive/portfolio.js
@@ -21,15 +21,17 @@
 					}
 					return columnNumber;
 				}
-				$(window).on('resize',function(){
+				function Resize(){
 					var width = $(window).width()
+					console.log(width)
 					var itemWidth = Math.floor(width / $scope.getColumnNumber());
 					$('#isotope').find('.element-item').each(function(){
-							$(this).css({width:itemWidth + 'px'})
+						$(this).css({width:itemWidth + 'px'})
 					})
-				})
+				}
+				$(window).on('resize','portfolio',Resize)
 				$scope.$on('$destroy',function(){
-					$(window).off('resize');
+					$(window).off('resize','portfolio');
 				})
 				$http.get('/api/common/promotiontypes').success(
 					function(response) {
@@ -45,31 +47,31 @@
 								})
 							}
 							setTimeout(function(){
-					                    // 绑定filter
-					                    var elm = angular.element('.filters');
-					                    elm.find('*[isotope-filter-by]').bind('click', function (i, e) {
-					                        angular.element('.filters .active').removeClass('active');
-					                        angular.element(this).addClass('active');
-					                        var filter = angular.element(this).attr('isotope-filter-by');
-					                        $scope.$broadcast('filter-elements', { filter: filter });
-					                    });
-					                    var option = {
-					                    	itemSelector:'.element-item',
-					                    	layoutMode:'fitRows'
-					                    }
-					                    // 绑定Item与事件
-					                    var elm2 = angular.element('#isotope')
-					                    elm2.isotope(option);
-					                    $scope.$watch('items',function(){
-					                    	setTimeout(function(){
-					                    		elm2.isotope('reloadItems').isotope(option)
-					                    	})
-					                    },true)
-					                    $scope.$on('filter-elements',function(data,filter){
-					                    	option.filter = filter.filter;
-					                    	elm2.isotope(option)
-					                    })
-					                    $(window).resize();
+								// 绑定filter
+								$(window).resize()
+								var elm = angular.element('.filters');
+								elm.find('*[isotope-filter-by]').bind('click', function (i, e) {
+									angular.element('.filters .active').removeClass('active');
+									angular.element(this).addClass('active');
+									var filter = angular.element(this).attr('isotope-filter-by');
+									$scope.$broadcast('filter-elements', { filter: filter });
+								});
+								var option = {
+									itemSelector:'.element-item',
+									layoutMode:'fitRows'
+								}
+								// 绑定Item与事件
+								var elm2 = angular.element('#isotope')
+								elm2.isotope(option);
+								$scope.$watch('items',function(){
+									setTimeout(function(){
+										elm2.isotope('reloadItems').isotope(option)
+									})
+								},true);
+								$scope.$on('filter-elements',function(data,filter){
+									option.filter = filter.filter;
+									elm2.isotope(option)
+								});
 							},500)
 						}
 				});
